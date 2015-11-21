78273c9b379ce25d3c69692d0ccaaa4442d3b454
Curtis Maloney <curtis@tinbrain.net>
2015-11-21 15:52:51 +1100

    Play nice with migrations

--- array_tags/fields.py
+++ array_tags/fields.py
@@ -9,31 +9,8 @@ from .lookups import Unnest
 class TagField(ArrayField):
     def __init__(self, **kwargs):
         kwargs.setdefault('blank', True)
-        super(TagField, self).__init__(models.CharField(max_length=50), **kwargs)
-
-    def contribute_to_class(self, cls, name, **kwargs):
-        super(TagField, self).contribute_to_class(cls, name, **kwargs)
-        # - tag with counts
-        # - similar items
-
-        def all_FOO_values(cls, name=name):
-            return (
-                cls.objects
-                .annotate(_v=Unnest(name))
-                .values_list('_v', flat=True)
-                .distinct()
-            )
-        setattr(cls, 'all_%s_values' % name, classmethod(all_FOO_values))
-
-        def count_FOO_values(cls, name=name):
-            return dict(
-                cls.objects
-                .annotate(_v=Unnest(name))
-                .values('_v')
-                .annotate(count=models.Count('*'))
-                .values_list('_v', 'count')
-            )
-        setattr(cls, 'count_%s_values' % name, classmethod(count_FOO_values))
+        kwargs.setdefault('base_field', models.CharField(max_length=50))
+        super(TagField, self).__init__(**kwargs)
 
     def pre_save(self, model_instance, add):
         '''
