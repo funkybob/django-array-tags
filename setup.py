4dc28bdb64fa42b092b8ca4bfb27faa8ff6213d7
Curtis Maloney <curtis@tinbrain.net>
2015-11-21 15:33:04 +1100

    Add setup files

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-lazy-tags',
+    version='0.1',
+    description='Simple Tagging for Django using PG ArrayField',
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    url='http://github.com/funkybob/django-lazy-tags',
+    packages = find_packages(exclude=('tests*',)),
+    zip_safe=False,
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    requires = [
+        'Django (>=1.7)',
+    ],
+    install_requires = [
+        'Django>=1.7',
+    ],
+)
