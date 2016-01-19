9bdae7c8b134b9dff2403d58255d388cfdfc287c
Curtis Maloney <curtis@tinbrain.net>
2016-01-19 12:31:32 +1100

    Add tests for most_like.  Fix old tests.

--- tests/test.py
+++ tests/test.py
@@ -10,23 +10,26 @@ all_tags = set(tags1 + tags2)
 tag_counts = Counter(tags1)
 tag_counts.update(tags2)
 
+
 class LazyTagTestCase(TestCase):
 
     def setUp(self):
-        m = TestModel.objects.create(tags=tags1)
-        n = TestModel.objects.create(tags=tags2)
-        
+        self.objects = [
+            TestModel.objects.create(tags=tags1),
+            TestModel.objects.create(tags=tags2)
+        ]
+
     def test_all_values(self):
-        v = TestModel.all_tags_values()
+        v = TestModel.objects.all_tag_values('tags')
         self.assertEqual(set(v), all_tags)
 
     def test_count_values(self):
-        v = TestModel.count_tags_values()
+        v = TestModel.objects.count_tag_values('tags')
         for key, value in v.items():
             self.assertEqual(value, tag_counts[key])
 
     def test_cleanup(self):
-        a = TestModel.objects.create(tags=['a', ' a ', 'b', ' b'])
+        TestModel.objects.create(tags=['a', ' a ', 'b', ' b'])
         b = TestModel.objects.last()
         self.assertEqual(set(b.tags), set(['a', 'b']))
 
@@ -39,3 +42,10 @@ class LazyTagTestCase(TestCase):
         for key, val in v.items():
             self.assertTrue(key in tags2)
             self.assertEqual(val, 1)
+
+    def test_most_like(self):
+        qset = list(TestModel.objects.most_like('tags', ['one', 'five']))
+        self.assertEqual(qset[0].similarity, 2)  # one, two
+        self.assertEqual(qset[0].pk, self.objects[1].pk)
+        self.assertEqual(qset[1].similarity, 1)  # one
+        self.assertEqual(qset[1].pk, self.objects[0].pk)
