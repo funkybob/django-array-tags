51a5422e4bc934575c637f3f4ad6708b50a0ae8c
Curtis Maloney <curtis@tinbrain.net>
2016-02-22 09:36:31 +1100

    Bumped version to 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-array-tags',
-    version='0.1.6',
+    version='0.1.7',
     description='Simple Tagging for Django using PG ArrayField',
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
