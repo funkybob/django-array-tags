cb48a14070d5a34c601d34d7161a48e0a89f892c
Curtis Maloney <curtis@tinbrain.net>
2017-07-15 12:02:46 +1000

    Tidy with jshint

--- array_tags/static/admin/js/array-tag.js
+++ array_tags/static/admin/js/array-tag.js
@@ -5,10 +5,10 @@
         this.el = (typeof el === 'string') ? document.querySelector(el) : el;
         this.el.style.display = 'none';
 
-        this.delim = this.el.dataset['delim'] || ',';
+        this.delim = this.el.dataset.delim || ',';
         this.values = new Set(this.split_tags(this.el.value));
 
-        this.el.closest('form').addEventListener('submit', (ev) => {
+        this.el.closest('form').addEventListener('submit', ev => {
             this.el.style.display = undefined;
             this.el.value = Array.from(this.values).join(this.delim + ' ');
         });
@@ -23,8 +23,8 @@
         this.button = document.createElement('a');
         this.button.innerText = 'Add';
         this.button.classList.add('button', 'add-tag');
-        this.button.addEventListener('click', (ev) => {
-            this.split_tags(this.inp.value).forEach((val) => this.values.add(val));
+        this.button.addEventListener('click', ev => {
+            this.split_tags(this.inp.value).forEach(val => this.values.add(val));
             this.inp.value = '';
             this.render_tags();
         });
@@ -32,14 +32,14 @@
 
         this.tagList = document.createElement('p');
         this.tagList.classList.add('tags');
-        this.tagList.addEventListener('click', (ev) => {
+        this.tagList.addEventListener('click', ev => {
             if(!ev.target.matches('.tags a')) return;
             this.values.delete(ev.target.parentNode.innerText.trim());
             this.render_tags();
         });
         div.appendChild(this.tagList);
 
-        this.el.parentElement.insertBefore(div, this.el)
+        this.el.parentElement.insertBefore(div, this.el);
 
         this.render_tags();
     };
@@ -47,20 +47,20 @@
     ArrayTag.prototype.render_tags = function () {
         this.tagList.innerHTML = Array.from(this.values)
             .sort()
-            .map(function (val) {return '<span>' + val + '<a href="#" class="deletelink"></a></span>';})
+            .map(val => '<span>' + val + '<a href="#" class="deletelink"></a></span>')
             .join(' ');
     };
 
     ArrayTag.prototype.split_tags = function (value) {
-      return value.split(this.delim).map((x) => x.trim()).filter(Boolean);
-    }
+      return value.split(this.delim).map(x => x.trim()).filter(Boolean);
+    };
 
     window.ArrayTag = ArrayTag;
 
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', () => {
         Array.from(document.querySelectorAll('.array-tag')).forEach(el => new ArrayTag(el));
     });
-    $('formset:added', function (ev, row) {
+    $('formset:added', (ev, row) => {
         Array.from(row.querySelectorAll('.array-tag')).forEach(el => new ArrayTag(el));
     });
 })();
